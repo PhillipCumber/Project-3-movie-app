d44658ecd2f963c52350ae63ae10bce3f4bc8cd7
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-15 14:56:32 -0500

    made a test route in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-        <Home />
+      <div>
+          <Router>
+            <div>
+              <Switch> 
+
+                  <Route path="/movies" component={Home}></Route>
+
+
+              </Switch>
+            </div>
+          </Router>
       </div>
     );
   }
