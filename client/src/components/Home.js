db4d6cdcd3bf42b4692bc2c494febdf4d8bb1a5e
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-15 14:18:14 -0500

    successfully linked express to react

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 class Home extends Component {
+
+    state = {
+
+        movies: [],
+    }
+
+    getAllMovies = () => {
+
+        axios.get('/movies')
+
+             .then((res) => {
+
+                console.log(res)
+                this.setState({movies: res.data})
+             })
+    }
+
+    componentDidMount() {
+
+        this.getAllMovies()
+    }
+
     render() {
         return (
             <div>
-                <h1>Watup</h1>
+                {/* <h1>Watup</h1>
+
+                {this.state.users.map((movie, i) => {
+
+                       {movie.name}
+                    
+                })} */}
             </div>
         );
     }
