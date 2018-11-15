705c39536a145841c7d7d8d9ffaadb9feb54e6fd
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-15 11:47:54 -0500

    created the home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Watup</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
