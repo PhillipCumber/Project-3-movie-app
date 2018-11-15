041f01024696a16cd45fe366e69cd1319d274ee9
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-15 09:39:19 -0500

    created seeds file for both my models

---
+++ db/seeds.js
@@ -0,0 +1,107 @@
+let User = require('../models/User')
+let Movie = require('../models/Movie')
+let mongoose = require('./connections')
+
+///Seeds for User model
+let user1 = new User({
+
+    name: 'Phillip Cumberlander',
+    img: '',
+    fav: 'Space Jam',
+    email: 'cumberlander11@gmail.com',
+    password: '1234'
+
+})
+
+////Seeds for Movie model
+
+let movie1 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie2 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie3 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie4 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie5 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie6 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie7 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie8 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie9 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
+let movie10 = new Movie ({
+
+    title: 'Space Jam',
+    img: '',
+    actors: 'Michael Jordan',
+    description: 'Michael Jordan being fye',
+    rating: 5
+})
+
