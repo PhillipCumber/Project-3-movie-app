d713241b5ac30f8f01f73742c2755f84f95b5a96
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-15 10:21:54 -0500

    added a route to show all movies

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 let express = require('express')
 let router = express.Router()
 let userController = require('../controllers/userController')
+let movieController = require('../controllers/movieController')
 
 ///routes for user model
 router.get('/', userController.index)
@@ -8,6 +9,6 @@ router.get('/members', userController.show)
 
 ///routes for movie model
 
-
+router.get('/movies', movieController.show)
 
 module.exports = router
