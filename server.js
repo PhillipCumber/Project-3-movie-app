40df1defdcb3f158fcfcd0960f8080e89783a67b
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-15 11:23:54 -0500

    added some middleware that connects express to react

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', routes)
 
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
 
 let PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
