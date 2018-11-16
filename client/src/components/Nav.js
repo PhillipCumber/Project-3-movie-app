afd2f808664bc69e94dc1c12db35fb4a00b74e56
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-16 10:11:54 -0500

    finished the navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+
+let NavStyle = styled.div`
+
+    display: flex;
+    justify-content: space-around;
+    font-size: 30px;
+    text-decoration: none;
+    text: green
+    list-style-type: none;
+`
 
 class Nav extends Component {
     render() {
         return (
             <div>
-                <div>
+                <NavStyle>
                     
-                    <li>Home</li>
+                   <Link to="/movies"><li>Movies</li></Link>
                     <li>Members</li>
                     <li>Movies</li>
                     <li>Test</li>
-                </div>
+                </NavStyle>
             </div>
         );
     }
