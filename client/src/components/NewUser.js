b9bca5207adb4cb49762d87b56a3e545708a997d
Phillip Cumberlander <cumberlander11@gmail.com>
2018-11-17 10:56:36 -0500

    added all my necessary components

--- client/src/components/NewUser.js
+++ client/src/components/NewUser.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class User extends Component {
+class NewUser extends Component {
     render() {
         return (
             <div>
-                <h1>User page</h1>
+                <h1>New user form</h1>
             </div>
         );
     }
 }
 
-export default User;
+export default NewUser;
